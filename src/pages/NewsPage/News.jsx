e826eece2b4e5ff014ad6eae03901c7276d58727
jonayed-nasir <junayednasir073@gmail.com>
2025-09-20 21:24:14 +0600

    news

--- src/pages/NewsPage/News.jsx
+++ src/pages/NewsPage/News.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function News() {
   return (
     <div>
-      <div  className='text-white'>This is News page</div>
+      <div  className='text-white'>This is News Page</div>
     </div>
   )
 }
